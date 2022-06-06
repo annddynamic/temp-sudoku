fbd5e1df1d513c66eee695d606b2ad441af5b18b
Andi Dika <andi.dika@student.uni-pr.edu>
2022-06-06 03:26:24 +0200

    S

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from "react-bootstrap";
 function App() {
   const Ref = useRef(null);
 
-  // The state for our timer
+  // // The state for our timer
   const [timer, setTimer] = useState("00:00:00");
 
   const getTimeRemaining = (e) => {
@@ -22,64 +22,64 @@ function App() {
     };
   };
 
-  const startTimer = (e) => {
-    let { total, hours, minutes, seconds } = getTimeRemaining(e);
-    if (total >= 0) {
-      // update the timer
-      // check if less than 10 then we need to
-      // add '0' at the begining of the variable
-      setTimer(
-        (hours > 9 ? hours : "0" + hours) +
-          ":" +
-          (minutes > 9 ? minutes : "0" + minutes) +
-          ":" +
-          (seconds > 9 ? seconds : "0" + seconds)
-      );
-    }
-  };
+  // const startTimer = (e) => {
+  //   let { total, hours, minutes, seconds } = getTimeRemaining(e);
+  //   if (total >= 0) {
+  //     // update the timer
+  //     // check if less than 10 then we need to
+  //     // add '0' at the begining of the variable
+  //     setTimer(
+  //       (hours > 9 ? hours : "0" + hours) +
+  //         ":" +
+  //         (minutes > 9 ? minutes : "0" + minutes) +
+  //         ":" +
+  //         (seconds > 9 ? seconds : "0" + seconds)
+  //     );
+  //   }
+  // };
 
-  const clearTimer = (e) => {
-    // If you adjust it you should also need to
-    // adjust the Endtime formula we are about
-    // to code next
-    setTimer("00:00:10");
+  // const clearTimer = (e) => {
+  //   // If you adjust it you should also need to
+  //   // adjust the Endtime formula we are about
+  //   // to code next
+  //   setTimer("00:00:10");
 
-    // If you try to remove this line the
-    // updating of timer Variable will be
-    // after 1000ms or 1sec
-    if (Ref.current) clearInterval(Ref.current);
-    const id = setInterval(() => {
-      startTimer(e);
-    }, 1000);
-    Ref.current = id;
-  };
+  //   // If you try to remove this line the
+  //   // updating of timer Variable will be
+  //   // after 1000ms or 1sec
+  //   if (Ref.current) clearInterval(Ref.current);
+  //   const id = setInterval(() => {
+  //     startTimer(e);
+  //   }, 1000);
+  //   Ref.current = id;
+  // };
 
-  const getDeadTime = () => {
-    let deadline = new Date();
+  // const getDeadTime = () => {
+  //   let deadline = new Date();
 
-    // This is where you need to adjust if
-    // you entend to add more time
-    deadline.setSeconds(deadline.getSeconds() + 10);
-    return deadline;
-  };
+  //   // This is where you need to adjust if
+  //   // you entend to add more time
+  //   deadline.setSeconds(deadline.getSeconds() + 10);
+  //   return deadline;
+  // };
 
-  useEffect(() => {
-    clearTimer(getDeadTime());
-  }, []);
+  // useEffect(() => {
+  //   clearTimer(getDeadTime());
+  // }, []);
 
-  // Another way to call the clearTimer() to start
-  // the countdown is via action event from the
-  // button first we create function to be called
-  // by the button
-  const onClickReset = () => {
-    clearTimer(getDeadTime());
-  };
+  // // Another way to call the clearTimer() to start
+  // // the countdown is via action event from the
+  // // button first we create function to be called
+  // // by the button
+  // const onClickReset = () => {
+  //   clearTimer(getDeadTime());
+  // };
 
 
   // const [matrix, setMatrix] = useState(Array(9).fill(Array(9)))
   const [matrix, setMatrix] = useState(
     [
-      [0,0,0,0,0,0,0,0,0],
+      [{v:0, isClicked:false},0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0],
@@ -94,6 +94,7 @@ function App() {
   
   const andi =(s, si)=>{
     console.log(s, si)
+    setClicked(!isClicked)
   }
 
   return (
@@ -101,28 +102,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <button onClick={onClickReset}>Reset</button>
+          <button>Reset</button>
           <span>{timer}</span>
         </div>
       </header>
       <Container>
         <Row className="p-5 ">
           <Col>
-            <Row xs={1} md={9}>
+            <Row style={{cursor:"pointer"}} xs={1} md={9}>
               {matrix.map((array, index)=>{
                 return (
                   <Col key={index}>
                     <Row md={9}>
                       {array.map((subarray, subindex)=>{
                         return(
-                          <Col onClick={(()=>andi(index, subindex))} key={subindex} className="andi">{subarray}</Col>
+                          <Col onClick={(()=>andi(index, subindex))} key={subindex} className={`andi ${isClicked ? 'clicked': ''}`}>{subarray}</Col>
                         )
                       })}
                     </Row>
                   </Col>
                 )
               })}
-              </Row>
+            </Row>
           </Col>
           <Col></Col>
         </Row>

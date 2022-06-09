38d5e3469fa45de4aa1e927631adbb151e4f3d5a
Andi Dika <andi.dika@student.uni-pr.edu>
2022-06-09 11:14:56 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col, Button, Alert, Dropdown } from "react-bootstrap";
 import Sudoku, { randomGenerator } from "./sudoku";
 
-
 function App() {
-  
   useEffect(() => {
     Sudoku.newGame(30);
     setBoard(Sudoku.getSolvedBoard());
     setMatrix(Sudoku.getUnsolvedBoard());
-    setCurrentDifficulty(30)
+    setCurrentDifficulty(30);
     setToFind(30);
     setLifes(5);
-    setShow(false)
+    setShow(false);
   }, []);
-  
+
   const [matrix, setMatrix] = useState([]);
   const [board, setBoard] = useState([]);
   const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
@@ -26,10 +24,10 @@ function App() {
   const [show, setShow] = useState(false);
   const [message, setMessage] = useState("");
   const [guess, setGuess] = useState("danger");
-  const [currentDifficulty, setCurrentDifficulty]= useState()
+  const [currentDifficulty, setCurrentDifficulty] = useState();
 
   console.log(board, matrix);
-  function appendToMatrix(a, b, number)  {
+  function appendToMatrix(a, b, number) {
     let newState = [];
     for (let i = 0; i < matrix.length; i++) {
       if (i === a) {
@@ -42,9 +40,9 @@ function App() {
       newState.push(matrix[i]);
     }
     setMatrix(newState);
-  };
+  }
 
-  function notify(message, guess){
+  function notify(message, guess) {
     setShow(true);
     setMessage(message);
     setGuess(guess);
@@ -60,56 +58,55 @@ function App() {
           //check if correct
           if (board[cords[0]][cords[1]] === number) {
             // notify user guess correct
-            notify("Correct!", "success")
-            
+            notify("Correct!", "success");
+
             appendToMatrix(cords[0], cords[1], number);
             setToFind((prevState) => prevState - 1);
             if (toFind === 1) {
               // Game is won, notify user
-              notify("Congratulations, you won!", "success")
+              notify("Congratulations, you solved the sudoku!", "success");
             }
           } else {
             // tell user wrong answer
-            notify("Wrong answer!", "danger")
+            notify("Wrong!", "danger");
             if (lifes > 1) {
               setLifes((prevState) => prevState - 1);
-            }else{
+            } else {
               // game over, notify user
               setLifes((prevState) => prevState - 1);
-              notify("You lost! Try again!", "danger")
+              notify("You lost! Try again!", "danger");
             }
           }
         } else {
           // notify user to select box
-          notify("Select an empty box in order to play!", "danger")
+          notify("Select an empty box in order to play!", "danger");
         }
-      }else{
+      } else {
         // notify user to select empty box
-        notify("Select an empty box in order to play!", "danger")
+        notify("Select an empty box in order to play!", "danger");
       }
     }
-  };
+  }
 
-  function newGame(currentDifficulty){
+  function newGame(currentDifficulty) {
     Sudoku.newGame(currentDifficulty);
-    setToFind(currentDifficulty)
+    setToFind(currentDifficulty);
     setBoard(Sudoku.getSolvedBoard());
     setMatrix(Sudoku.getUnsolvedBoard());
     setLifes(5);
-    setShow(false)
-  };
+    setShow(false);
+  }
 
   function changeDifficulty(difficulty) {
-    setCurrentDifficulty(difficulty)
-    newGame(difficulty)
-    setShow(false)
-  };
-
+    setCurrentDifficulty(difficulty);
+    newGame(difficulty);
+    setShow(false);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo"   alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h2>Lifes left: {lifes} out of 5</h2>
         <div>
           <Dropdown>
@@ -117,14 +114,23 @@ function App() {
               Difficulty
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item onClick={()=>changeDifficulty(1)} >Easy</Dropdown.Item>
-              <Dropdown.Item onClick={()=>changeDifficulty(2)} >Medium</Dropdown.Item>
-              <Dropdown.Item onClick={()=>changeDifficulty(3)} >Hard</Dropdown.Item>
+              <Dropdown.Item onClick={() => changeDifficulty(1)}>
+                Easy
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => changeDifficulty(2)}>
+                Medium
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => changeDifficulty(3)}>
+                Hard
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </div>
       </header>
       <Container>
+        <Alert className="mt-5" show={show} variant={guess}>
+          <Alert.Heading>{message}</Alert.Heading>
+        </Alert>
         <Row className="p-5 ">
           <Col>
             <Row
@@ -159,7 +165,10 @@ function App() {
           </Col>
           <Col>
             <Container>
-              <Button className="form-control " onClick={()=>newGame(currentDifficulty)}>
+              <Button
+                className="form-control "
+                onClick={() => newGame(currentDifficulty)}
+              >
                 New Game
               </Button>
               <Row md={3}>
@@ -177,9 +186,6 @@ function App() {
               </Row>
             </Container>
           </Col>
-          <Alert show={show} variant={guess}>
-            <Alert.Heading>{message}</Alert.Heading>
-          </Alert>
         </Row>
       </Container>
     </div>
